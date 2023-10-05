256ca49c550ce2d2d8a7acd78cad842efb578380
Ramzi BENMANSOUR <ramzibenmansour27@gmail.com>
2023-10-05 11:02:39 +0200

    FirstLast6 done

---
+++ src/fr/algorithmie/FirstLast6.java
@@ -0,0 +1,15 @@
+package fr.algorithmie;
+
+public class FirstLast6 {
+
+	public static void main(
+			String[] args) {
+		int[] tableau = {3, 4, 6, 1, 7, 5, 11 };
+		
+        boolean resultat = tableau.length > 0 && (tableau[0] == 6 || tableau[tableau.length - 1] == 6);
+        System.out
+				.println("resultat : " + resultat);
+
+	}
+
+}
