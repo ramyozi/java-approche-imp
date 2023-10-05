3acc0c4564901da75df4832b1c19a2a7b5d7d822
Ramzi BENMANSOUR <ramzibenmansour27@gmail.com>
2023-10-05 11:43:40 +0200

    Surround Scanner with a try catch in InteractifChiffresSuivants class

--- src/fr/algorithmie/InteractifChiffresSuivants.java
+++ src/fr/algorithmie/InteractifChiffresSuivants.java
@@ -5,18 +5,19 @@ import java.util.Scanner;
 public class InteractifChiffresSuivants {
 
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("enter a number : ");
-		int nb = scanner.nextInt();
-		System.out.println("the next 10 numbers are : ");
-		for (int i = 1; i <= 10; i++) {
-			if (i != 10) {
-				System.out.print(nb + i + ", ");
-			} else {
-				System.out.print(nb + i);
+		try (Scanner scanner = new Scanner(System.in)) {
+			System.out.println("enter a number : ");
+			int nb = scanner.nextInt();
+			System.out.println("the next 10 numbers are : ");
+			for (int i = 1; i <= 10; i++) {
+				if (i != 10) {
+					System.out.print(nb + i + ", ");
+				} else {
+					System.out.print(nb + i);
+
+				}
 
 			}
-
 		}
 
 	}
