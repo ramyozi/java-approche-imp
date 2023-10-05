985cec1f6ebbda6eeb3f0c2ac4dcb7a2ef48e932
Ramzi BENMANSOUR <ramzibenmansour27@gmail.com>
2023-10-05 12:53:56 +0200

    InteractifFibonacci updated

--- src/fr/algorithmie/InteractifFibonacci.java
+++ src/fr/algorithmie/InteractifFibonacci.java
@@ -7,18 +7,33 @@ public class InteractifFibonacci {
 	public static void main(String[] args) {
 
 		try (Scanner scanner = new Scanner(System.in)) {
-            System.out.println("Enter a number: " );
-			int number = scanner.nextInt();
+            System.out.print("Enter position N in the Fibonacci sequence : ");
+            int n = scanner.nextInt();
 
-			
-            double a = Math.pow(1 + Math.sqrt(5), number);
-            double b = Math.pow(2, number) * Math.sqrt(5);
-            
-            double result = a / b;
+            if (n < 0) {
+                System.out.println("N can't be a negative number.");
+            } else {
+                long result = fibonacci(n);
+                System.out.println("The number at position N in the Fibonacci sequence is : " + result);
+            }
+        }
+    }
 
-            System.out.println("Result: " + result);
+    public static long fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        }
 
-		}
+        long[] fibArray = new long[n + 1];
+        fibArray[0] = 0;
+        fibArray[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            fibArray[i] = fibArray[i - 1] + fibArray[i - 2];
+        }
+
+        return fibArray[n];
+    }
 	}
 
-}
+
