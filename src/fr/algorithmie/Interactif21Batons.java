39aab0bcf31d94e09ded55b673dc7aee439b9988
Ramzi BENMANSOUR <ramzibenmansour27@gmail.com>
2023-10-06 10:00:13 +0200

    Adding displayBâtons method to Interactif21Batons

--- src/fr/algorithmie/Interactif21Batons.java
+++ src/fr/algorithmie/Interactif21Batons.java
@@ -14,15 +14,16 @@ public class Interactif21Batons {
             System.out.println("Bienvenue au jeu des 21 Bâtons !");
             System.out.println("Règles : Prenez 1 à 3 bâtons à tour de rôle. Celui qui prend le dernier bâton perd.");
             System.out.println("Nombre de bâtons au départ : " + batons);
-
+            
             while (batons > 0) {
                 int maxRetrait = Math.min(3, batons); 
+                displayBâtons(batons);
+
                 if (tourJoueur) {
                     System.out.print("C'est votre tour. Combien de bâtons voulez-vous prendre (1-" + maxRetrait + ") ? ");
                     int choixJoueur = scanner.nextInt();
                     if (choixJoueur >= 1 && choixJoueur <= maxRetrait) {
                         batons -= choixJoueur;
-                        System.out.println("Vous avez pris " + choixJoueur + " bâton(s). Il reste " + batons + " bâtons.");
                         tourJoueur = false;
                     } else {
                         System.out.println("Choix invalide. Veuillez choisir un nombre entre 1 et " + maxRetrait + ".");
@@ -31,7 +32,6 @@ public class Interactif21Batons {
                     int choixOrdinateur = random.nextInt(maxRetrait) + 1;
                     System.out.println("C'est le tour de l'ordinateur. Il prend " + choixOrdinateur + " bâton(s).");
                     batons -= choixOrdinateur;
-                    System.out.println("Il reste " + batons + " bâtons.");
                     tourJoueur = true;
                 }
             }
@@ -42,5 +42,14 @@ public class Interactif21Batons {
                 System.out.println("Désolé, vous avez perdu ! Vous avez pris le dernier bâton.");
             }
         }
+    }
+
+    private static void displayBâtons(int count) {
+        System.out.print("Bâtons restants: ");
+        for (int i = 0; i < count; i++) {
+            System.out.print("| ");
+        }
+        System.out.print(" (" + count + ") ");
+        System.out.println(); 
     }
 }
